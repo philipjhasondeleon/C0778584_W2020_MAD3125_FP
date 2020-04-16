97da0b7b6c042a6458b2331c5e1f4bcce75034a2
Philip <jhasondeleon@gmail.com>
2020-04-15 20:27:01 -0400

    [Philip] hydro extends to bill class

--- app/src/main/java/com/example/c0778584_w2020_mad3125_fp/model/Hydro.java
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_fp/model/Hydro.java
@@ -1,6 +1,49 @@
 package com.example.c0778584_w2020_mad3125_fp.model;
 
-public class Hydro {
+public class Hydro extends Bill{
     private String agencyName;
     private Integer unitsUsed;
+
+    public Hydro(String billId, String billDate, BillType billType, String agencyName, Integer unitsUsed){
+        super(billId, billDate, billType);
+        this.agencyName = agencyName;
+        this.unitsUsed = unitsUsed;
+        this.billTotal = billCalculate();
+    }
+
+    @Override
+    public Double billCalculate()
+    {
+        double billAmount = 0.0;
+        if (unitsUsed < 10)
+        {
+            billAmount = 1.5 * unitsUsed;
+        }
+        else
+        {
+            billAmount = 2 * unitsUsed;
+        }
+        return billAmount;
+    }
+
+    public String getAgencyName()
+    {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName)
+    {
+        this.agencyName = agencyName;
+    }
+
+    public Integer getUnitsUsed()
+    {
+        return unitsUsed;
+    }
+
+    public void setUnitsUsed(Integer unitsUsed)
+    {
+        this.unitsUsed = unitsUsed;
+    }
+
 }
