f191a3fb159c762ffb6d2a971407e39631e2b235
Philip <jhasondeleon@gmail.com>
2020-04-15 20:53:39 -0400

    [Philip] Mobile extends to bill class

--- app/src/main/java/com/example/c0778584_w2020_mad3125_fp/model/Mobile.java
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_fp/model/Mobile.java
@@ -1,9 +1,78 @@
 package com.example.c0778584_w2020_mad3125_fp.model;
 
-public class Mobile {
+public class Mobile extends Bill
+{
     private String manufacturerName;
     private String planName;
     private String mobileNumber;
     private Integer mobGbUsed;
     private Integer minute;
+
+    public Mobile(String billId, String billDate, BillType billType, String manufacturerName, String planName, String mobileNumber, Integer mobGbUsed, Integer minute){
+        super(billId, billDate, billType);
+        this.manufacturerName = manufacturerName;
+        this.planName = planName;
+        this.mobileNumber = mobileNumber;
+        this.mobGbUsed = mobGbUsed;
+        this.minute = minute;
+        this.billTotal = billCalculate();
+    }
+
+    //Overriding default implementation of billCalculate in Bill class
+    @Override
+    public Double billCalculate(){
+        double billAmount = 0.0;
+        billAmount = (this.mobGbUsed) * 25 + (this.minute) * 0.2;
+        return billAmount;
+    }
+
+    public String getManufacturerName()
+    {
+        return manufacturerName;
+    }
+
+    public void setManufacturerName(String manufacturerName)
+    {
+        this.manufacturerName = manufacturerName;
+    }
+
+    public String getPlanName()
+    {
+        return planName;
+    }
+
+    public void setPlanName(String planName)
+    {
+        this.planName = planName;
+    }
+
+    public String getMobileNumber()
+    {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber)
+    {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Integer getMobGbUsed()
+    {
+        return mobGbUsed;
+    }
+
+    public void setMobGbUsed(Integer mobGbUsed)
+    {
+        this.mobGbUsed = mobGbUsed;
+    }
+
+    public Integer getMinute()
+    {
+        return minute;
+    }
+
+    public void setMinute(Integer minute)
+    {
+        this.minute = minute;
+    }
 }
