c79b5daaf5a30ee72fc424fed40ed2d6e807600e
Philip <jhasondeleon@gmail.com>
2020-04-15 20:48:56 -0400

    [Philip] Internet extends to bill class

--- app/src/main/java/com/example/c0778584_w2020_mad3125_fp/model/Internet.java
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_fp/model/Internet.java
@@ -1,6 +1,47 @@
 package com.example.c0778584_w2020_mad3125_fp.model;
 
-public class Internet {
+public class Internet extends Bill {
     private String providerName;
     private Double gbUsed;
+
+    public Internet(String billId, String billDate, BillType billType, String providerName, Double gbUsed)
+    {
+        super(billId, billDate, billType);
+        this.providerName = providerName;
+        this.gbUsed = gbUsed;
+        this.billTotal = billCalculate();
+    }
+
+    @Override
+    public Double billCalculate()
+    {
+        double billAmount = 0.0;
+        if( gbUsed < 10){
+            billAmount = 3 * gbUsed;
+        }
+        else {
+            billAmount = 3.5 * gbUsed;
+        }
+        return billAmount;
+    }
+
+    public String getProviderName()
+    {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName)
+    {
+        this.providerName = providerName;
+    }
+
+    public Double getGbUsed()
+    {
+        return gbUsed;
+    }
+
+    public void setGbUsed(Double gbUsed)
+    {
+        this.gbUsed = gbUsed;
+    }
 }
