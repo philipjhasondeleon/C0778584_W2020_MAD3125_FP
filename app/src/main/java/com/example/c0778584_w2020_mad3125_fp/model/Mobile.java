deff1491b31fbcba2d38218572f97d55d2dc0106
Philip <jhasondeleon@gmail.com>
2020-04-15 20:15:34 -0400

    [Philip] mobile class added

---
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_fp/model/Mobile.java
@@ -0,0 +1,9 @@
+package com.example.c0778584_w2020_mad3125_fp.model;
+
+public class Mobile {
+    private String manufacturerName;
+    private String planName;
+    private String mobileNumber;
+    private Integer mobGbUsed;
+    private Integer minute;
+}
