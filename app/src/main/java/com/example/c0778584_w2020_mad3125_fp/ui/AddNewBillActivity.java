d4496a4e5e968602d3360030b2054d7a4c757f6a
Philip <jhasondeleon@gmail.com>
2020-04-17 20:58:58 -0400

    [Philip] AddNewBillActivity Created

---
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_fp/ui/AddNewBillActivity.java
@@ -0,0 +1,14 @@
+package com.example.c0778584_w2020_mad3125_fp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_bill);
+    }
+}
