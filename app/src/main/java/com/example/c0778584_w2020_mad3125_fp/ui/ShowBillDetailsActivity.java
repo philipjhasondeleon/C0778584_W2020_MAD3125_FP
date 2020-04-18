1c7b7a02db8a8fbe0c983d885391d00cd85710b5
Philip <jhasondeleon@gmail.com>
2020-04-17 21:35:49 -0400

    [Philip] AddnNewBillDetails button added

--- app/src/main/java/com/example/c0778584_w2020_mad3125_fp/ui/ShowBillDetailsActivity.java
+++ app/src/main/java/com/example/c0778584_w2020_mad3125_fp/ui/ShowBillDetailsActivity.java
@@ -3,6 +3,8 @@ package com.example.c0778584_w2020_mad3125_fp.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.c0778584_w2020_mad3125_fp.R;
 
@@ -12,5 +14,11 @@ public class ShowBillDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_bill_details);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
     }
 }
